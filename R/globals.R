22e6ebb929919efdcdce8bac6c89baddbb3144dd
yeyuan98 <yuanye1998@gmail.com>
2024-08-11 18:35:57 -0400

    Initial commit

---
+++ R/globals.R
@@ -0,0 +1,8 @@
+# Variable list for dplyr operations
+
+utils::globalVariables(c(
+  # ABEtools
+  c("targetIdx", "subjectHits", "variant_ids"),
+  c("variant_id", "variant_data", "mutation.perc.median"),
+  c("name", "REF", "ALT", "countREF", "countALT")
+))
